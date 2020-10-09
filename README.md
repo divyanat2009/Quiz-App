94637947404872358355bfa85c4da31f19bdf880
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-09 14:10:31 -0500

    Wireframe for Quiz App.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# Quiz-App" 
+
+This is a app that features 6 questions. 
+The app uses even listeners to check correct answers.
+
--- questions.js
+++ questions.js
@@ -1,1 +1,61 @@
-ECHO is on.
+const quiz ={ 
+    
+questions:[
+{
+    question:'What is anorexia caused by?',
+    answers:[
+        'Not enough energy intake',
+        'Too much fat',
+        'Not enough protein',
+        'Not enough exercise'],
+    correctAnswer: 'Not enough energy intake' 
+},
+{
+    question:'A deficiency of iron in the diet could lead to what?',
+    answers:[
+        'Scurvy',
+        'Pellagra',
+        'Anaemia',
+        'Skin problems'],
+    correctAnswer: 'Anaemia' 
+},
+{
+    question:'Which one has the highest energy content?',
+    answers:[
+        'Protein',
+        'Fat',
+        'Sugar',
+        'Starch'],
+    correctAnswer: 'Fat' 
+},
+{
+    question:'A deficiency of iron in the diet could lead to what?',
+    answers:[
+        'Scurvy',
+        'Pellagra',
+        'Anaemia',
+        'Skin problems'],
+    correctAnswer: 'Anaemia' 
+},
+{
+    question:'A diet which is low in fibre could lead to which disorder?',
+    answers:[
+        'Heart Disease',
+        'Bowel Cancer',
+        'Diabetes',
+        'Cystic Fibrosis'],
+    correctAnswer: 'Bowel Cancer' 
+},
+{
+    question:'A balanced diet must have carbohydrates, proteins, fat, fibre, vitamins and which other type of food?',
+    answers:[
+        'Fatty Acids',
+        'Cholestrol',
+        'Water',
+        'Minerals'],
+    correctAnswer: 'Minerals' 
+}    
+],
+questionNumber: 0,
+Score: 0
+};
