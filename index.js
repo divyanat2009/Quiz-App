320bfec81669d85e80d2a17d4d28c44f48ae1137
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-10-15 10:58:30 -0500

    removed css styling, moved questions.js, created styling for HUD

--- index.js
+++ index.js
@@ -1,112 +1,99 @@
-let questions = [
-  {
-      question:'What is anorexia caused by?',
-      answers: [
-      'A. Not enough energy intake',
-      'B. Too much fat',
-      'C. Not enough protein',
-      'D. Not enough exercise'],
-      correctAnswer: 0
-  },
-  { 
-      question:'A deficiency of iron in the diet could lead to what?',
-      answers: [
-      'A. Scurvy',
-      'B. Pellagra',
-      'C. Anaemia',
-      'D. Skin problems'],
-      correctAnswer: 3
-  },
-  {
-      question:'Which one has the highest energy content?',
-      answers:[
-       'A. Protein',
-       'B. Fat',
-       'C. Sugar',
-       'D. Starch'],
-      correctAnswer: 2
-  },
-  {
-      question:'Which of the following increases your metabolic rate?',
-      answers: [
-      'A. Eating',
-      'B. Excercise',
-      'C. Sleeping',
-      'D. Being Overweight'],
-      correctAnswer: 2
-  },
-  {
-      question:'A diet which is low in fibre could lead to which disorder?',
-      answers:[
-      'A. Heart Disease',
-      'B. Bowel Cancer',
-      'C. Diabetes',
-      'D. Cystic Fibrosis'],
-      correctAnswer: 2
-  },
-  {
-      question:'A balanced diet must have carbohydrates, proteins, fat, fibre, vitamins and which other type of food?',
-      answers:[
-      'A. Fatty Acids',
-      'B. Cholestrol',
-      'C. Water',
-      'D. Minerals'],
-      correctAnswer: 4
-  }   
-  ]
-//Assign Variables
-let score = 0;
-let questionCounter=0;
 
-//Start the quiz
-function startQuiz(){
-  renderQuestionsAndAnswers(questions[0].question, questions[0].answers);
-  renderNextQuestionAndAnswers();
+  // Initialize Variables
+  let score = 0;
+  let questionCounter = 0;
 
-}
-//Start by resetting score and question counter to 0
-function resetValues(){
-  questionCounter = 0;
-  score = 0;
-}
-//Update the question counter
-function updateQuestionCount(counter){
-  $('.#progress-text').html("Question" + counter + "of" + questions.length)
-}
-
-function onOffComponents(on){
-  if(!on)
-  {
-    $('#hud').hide();
-    $('.quiz-container').hide();
-    $('.result-container').hide();
+  //Reset question counter and score
+  function resetValues(){
+    questionCounter = 0;
+    score = 0;
   }
-  else
+  
+  function updateQuestionCount(counter)
   {
-    $('#hud').removeAttr('hidden');
-    $('.quiz-container').removeAttr('hidden');
-    $('.result-container').removeAttr('hidden');
+    $("#progressText").html("Question "+counter +" of " +questions.length);
   }
-}
- function renderQuestionsAndAnswers(question, answers){
-   $('#question').html = question;
-   answers.forEach(answer =>{
-   $('.choice-container').append(
-   `<li><input type ="radio" class="choice-text"> ${answer}</li>`)
-   });   
- }
- function renderNextQuestionAndAnswers(){
-    $('.questionForm').submit(function(e){
-    e.preventDefault();    
-    questionCounter++;
-    console.log(questions);
-    console.log(questions[questionCounter].question);
-    console.log(questions[questionCounter]["answers"]);
-   // renderQuestionsAndAnswers(questions[questionCounter].question, questions[questionCounter].answers);
-   });
-
- }
-
-startQuiz();
-
-
+  
+  // hide containers
+  function OnOffComponents(on)
+   {
+    if(!on)
+    {
+      $("#hud").hide();
+      $("#score").hide();
+      $(".quiz-container").hide();
+      $(".result-container").hide();
+    }
+    else
+    {
+      $("#hud").removeAttr("hidden");
+      $("#score").removeAttr("hidden");
+      $(".quiz-container").removeAttr("hidden");
+      $(".result-container").removeAttr("hidden");
+    }
+   }
+  
+   //render the question and respective answers each time
+   function renderQuestionAndAnswers(question, answers){
+     $('#question').html(question);
+     $('.choice-container').html("");
+     answers.forEach(answer =>{
+     $('.choice-container').append(
+     `<li><input type ="radio" class="choice-text"> ${answer}</li>`)
+     });   
+   }
+  //render next question and answers
+   function renderNextQuestionAndAnswers(){
+      $('.questionForm').submit(function(e){
+      e.preventDefault();    
+      questionCounter++;
+      if(questionCounter == questions.length)
+      {
+       alert("No more questions");
+       resetValuesAndRenderQuestionsAndAnswers();
+       init();
+      }
+      else
+      {
+        updateQuestionCount(questionCounter+1);
+        renderQuestionAndAnswers(questions[questionCounter].question, questions[questionCounter].answers);
+        
+      }
+      
+     });
+  
+   }
+  // when start button id clicked
+   function startQuiz()
+   {
+      $("#startQuiz").click(()=>{
+      OnOffComponents(true);
+      resetValuesAndRenderQuestionsAndAnswers();
+      renderNextQuestionAndAnswers();
+     
+    });
+   }
+  
+   function reStartQuiz(){
+    $(".restartQuiz").click(()=>{
+      resetValuesAndRenderQuestionsAndAnswers();
+      init();
+    });
+   }
+  
+   function resetValuesAndRenderQuestionsAndAnswers(){
+      resetValues();
+      renderQuestionAndAnswers(questions[questionCounter].question, questions[questionCounter].answers);
+   }
+  
+   //call all functions
+   function init()
+   {
+    startQuiz();
+    reStartQuiz();
+    resetValues();
+    updateQuestionCount(1);
+   }
+  $(init);
+  
+  
--- questions.js
+++ questions.js
@@ -1,55 +1,59 @@
-questions = [
-{
-    question:'What is anorexia caused by?',
-    answers: [
-    'Not enough energy intake',
-    'Too much fat',
-    'Not enough protein',
-    'Not enough exercise'],
-    correctAnswer: 0
-},
-{
-    question:'A deficiency of iron in the diet could lead to what?',
-    choice1: 'Scurvy',
-    choice2: 'Pellagra',
-    choice3: 'Anaemia',
-    choice4: 'Skin problems',
-    correctAnswer: 3
-},
-{
-    question:'Which one has the highest energy content?',
-    choice1: 'Protein',
-    choice2: 'Fat',
-    choice3: 'Sugar',
-    choice4: 'Starch',
-    correctAnswer: 2
-},
-{
-    question:'Which of the following increases your metabolic rate?',
-    choice1: 'Eating',
-    choice2: 'Excercise',
-    choice3: 'Sleeping',
-    choice4: 'Being Overweight',
-    correctAnswer: 2
-},
-{
-    question:'A diet which is low in fibre could lead to which disorder?',
-    choice1: 'Heart Disease',
-    choice2: 'Bowel Cancer',
-    choice3: 'Diabetes',
-    choice4: 'Cystic Fibrosis',
-    correctAnswer: 2
-},
-{
-    question:'A balanced diet must have carbohydrates, proteins, fat, fibre, vitamins and which other type of food?',
-    choice1: 'Fatty Acids',
-    choice2: 'Cholestrol',
-    choice3: 'Water',
-    choice4: 'Minerals',
-    correctAnswer: 4
-}    
-]
-
+let questions = [
+    {
+        question:'What is anorexia caused by?',
+        answers: [
+        'A. Not enough energy intake',
+        'B. Too much fat',
+        'C. Not enough protein',
+        'D. Not enough exercise'],
+        correctAnswer: 0
+    },
+    { 
+        question:'A deficiency of iron in the diet could lead to what?',
+        answers: [
+        'A. Scurvy',
+        'B. Pellagra',
+        'C. Anaemia',
+        'D. Skin problems'],
+        correctAnswer: 2
+    },
+    {
+        question:'Which one has the highest energy content?',
+        answers:[
+         'A. Protein',
+         'B. Fat',
+         'C. Sugar',
+         'D. Starch'],
+        correctAnswer: 1
+    },
+    {
+        question:'Which of the following increases your metabolic rate?',
+        answers: [
+        'A. Eating',
+        'B. Excercise',
+        'C. Sleeping',
+        'D. Being Overweight'],
+        correctAnswer: 1
+    },
+    {
+        question:'A diet which is low in fibre could lead to which disorder?',
+        answers:[
+        'A. Heart Disease',
+        'B. Bowel Cancer',
+        'C. Diabetes',
+        'D. Cystic Fibrosis'],
+        correctAnswer: 1
+    },
+    {
+        question:'A balanced diet must have carbohydrates, proteins, fat, fibre, vitamins and which other type of food?',
+        answers:[
+        'A. Fatty Acids',
+        'B. Cholestrol',
+        'C. Water',
+        'D. Minerals'],
+        correctAnswer: 3
+    }   
+    ]
 
 
 
